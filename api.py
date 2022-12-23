a43b2cd30c35d8e0df8f1f56fa467ea900b0e6aa
Aarin <92866904+aarin13@users.noreply.github.com>
2022-12-23 18:53:19 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ please install required dependencies first
 pip install fastapi
 pip install torch
 pip install --upgrade diffusers[torch]
-pip intsall transformers
+pip install transformers
 """
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
@@ -16,20 +16,35 @@ from diffusers import StableDiffusionPipeline
 from io import BytesIO
 import base64
 
+def dummy(images, **kwargs):
+    return images, False
+
 app = FastAPI()
 device = "cuda"
 model_id = "CompVis/stable-diffusion-v1-4"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, revision="fp16")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe.to(device)
 
+pipe.safety_checker = dummy
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_credentials=True, 
+    allow_origins=["*"], 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
+
 @app.get("/")
-def generate(prompt: str):
-    with autocast(device):
-        image = pipe(prompt, guidance_scale=8.5).images[0]
+def generate(prompt: str): 
+    with autocast(device): 
+        image = pipe(prompt, guidance_scale=8.5, height=256, width=256).images[0] #change height and width to 512 for use with powerful gpu
 
     image.save("testimage.png")
+    #image generated with nvidia 16XX gpu will only be a green image.
     buffer = BytesIO()
     image.save(buffer, format="PNG")
     imgstr = base64.b64encode(buffer.getvalue())
 
     return Response(content=imgstr, media_type="image/png")
+    
